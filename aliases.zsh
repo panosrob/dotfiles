65cbb95c687efa18637bd07a9d6d2734acff4091
Panagiotis Rompolas <prompolas@customedialabs.com>
2018-11-15 19:22:20 +0200

    change file names

--- aliases.zsh
+++ aliases.zsh
@@ -15,14 +15,8 @@
 #alias p="cd ~/projects"
 #alias g="git"
 
-# Detect which `ls` flavor is in use
-#if ls --color > /dev/null 2>&1; then # GNU `ls`
-#	colorflag="--color"
-#	export LS_COLORS='no=00:fi=00:di=01;31:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.avi=01;35:*.fli=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.ogg=01;35:*.mp3=01;35:*.wav=01;35:'
-#else # macOS `ls`
-#	colorflag="-G"
-#	export LSCOLORS='BxBxhxDxfxhxhxhxhxcxcx'
-#fi
+colorflag="-G"
+export LSCOLORS='BxexhxDxfxhxhxhxhxcxcx'
 
 # List all files colorized in long format
 alias l="ls -lF ${colorflag}"
@@ -53,7 +47,6 @@ alias update='sudo softwareupdate -i -a; brew update; brew upgrade; brew cleanup
 
 # Google Chrome
 alias chrome='/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
-#alias canary='/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary'
 
 # IP addresses
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
@@ -67,21 +60,7 @@ alias ifactive="ifconfig | pcregrep -M -o '^[^\t:]+:([^\n]|\n\t)*status: active'
 #alias flush="dscacheutil -flushcache && killall -HUP mDNSResponder"
 
 # Clean up LaunchServices to remove duplicates in the “Open With” menu
-alias lscleanup="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user && killall Finder"
-
-# Canonical hex dump; some systems have this symlinked
-#command -v hd > /dev/null || alias hd="hexdump -C"
-
-# macOS has no `md5sum`, so use `md5` as a fallback
-#command -v md5sum > /dev/null || alias md5sum="md5"
-
-# macOS has no `sha1sum`, so use `shasum` as a fallback
-#command -v sha1sum > /dev/null || alias sha1sum="shasum"
-
-# JavaScriptCore REPL
-#jscbin="/System/Library/Frameworks/JavaScriptCore.framework/Versions/A/Resources/jsc";
-#[ -e "${jscbin}" ] && alias jsc="${jscbin}";
-#unset jscbin;
+#alias lscleanup="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user && killall Finder"
 
 # Trim new lines and copy to clipboard
 #alias c="tr -d '\n' | pbcopy"
@@ -114,12 +93,6 @@ alias spotoff="sudo mdutil -a -i off"
 # Enable Spotlight
 alias spoton="sudo mdutil -a -i on"
 
-# PlistBuddy alias, because sometimes `defaults` just doesn’t cut it
-#alias plistbuddy="/usr/libexec/PlistBuddy"
-
-# Airport CLI alias
-alias airport='/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport'
-
 # Intuitive map function
 # For example, to list all directories that contain a certain file:
 # find . -name .gitattributes | map dirname
@@ -132,14 +105,6 @@ done
 
 # Make Grunt print stack traces by default
 command -v grunt > /dev/null && alias grunt="grunt --stack"
-
-# Stuff I never really use but cannot delete either because of http://xkcd.com/530/
-alias stfu="osascript -e 'set volume output muted true'"
-alias pumpitup="osascript -e 'set volume output volume 100'"
-
-# Kill all the tabs in Chrome to free up memory
-# [C] explained: http://www.commandlinefu.com/commands/view/402/exclude-grep-from-your-grepped-output-of-ps-alias-included-in-description
-#alias chromekill="ps ux | grep '[C]hrome Helper --type=renderer' | grep -v extension-process | tr -s ' ' | cut -d ' ' -f2 | xargs kill"
 
 # Lock the screen (when going AFK)
 alias afk="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
--- exports.zsh
+++ exports.zsh
@@ -1,18 +1,4 @@
 #!/usr/bin/env bash
-
-# Make vim the default editor.
-#export EDITOR='vim';
-
-# Enable persistent REPL history for `node`.
-#export NODE_REPL_HISTORY=~/.node_history;
-# Allow 32³ entries; the default is 1000.
-#export NODE_REPL_HISTORY_SIZE='32768';
-# Use sloppy mode by default, matching web browsers.
-#export NODE_REPL_MODE='sloppy';
-
-# Make Python use UTF-8 encoding for output to stdin, stdout, and stderr.
-#export PYTHONIOENCODING='UTF-8';
-
 # Increase Bash history size. Allow 32³ entries; the default is 500.
 export HISTSIZE='32768';
 export HISTFILESIZE="${HISTSIZE}";
@@ -26,20 +12,17 @@ export LC_ALL='en_US.UTF-8';
 # Highlight section titles in manual pages.
 export LESS_TERMCAP_md="${yellow}";
 
-# Don’t clear the screen after quitting a manual page.
-#export MANPAGER='less -X';
-
 # Avoid issues with `gpg` as installed via Homebrew.
 # https://stackoverflow.com/a/42265848/96656
 #export GPG_TTY=$(tty);
 
 # ncurses after zsh installation
 # For compilers to find ncurses you may need to set:
-export LDFLAGS="-L/usr/local/opt/ncurses/lib"
-export CPPFLAGS="-I/usr/local/opt/ncurses/include"
+export LDFLAGS="-L/usr/local/opt/ncurses/lib";
+export CPPFLAGS="-I/usr/local/opt/ncurses/include";
 
 # For pkg-config to find ncurses you may need to set:
-export PKG_CONFIG_PATH="/usr/local/opt/ncurses/lib/pkgconfig"
+export PKG_CONFIG_PATH="/usr/local/opt/ncurses/lib/pkgconfig";
 
-export PATH="/Users/panosrob/Library/Android/sdk/platform-tools:$PATH"
-export PATH="/Users/panosrob/Library/Python/3.7/bin:$PATH"
+export PATH="/Users/panosrob/Library/Android/sdk/platform-tools:$PATH";
+export PATH="/Users/panosrob/Library/Python/3.7/bin:$PATH";
